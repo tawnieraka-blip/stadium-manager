0f532883970dce8b3a68b6fc6bdf877487f6c6cc
tawnieraka-blip <tawnie.raka@gmail.com>
2026-07-01 20:14:51 +0300

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,29 @@
+const themeBtn=document.getElementById("themeBtn");
 
+if(localStorage.getItem("theme")==="dark"){
+
+    document.body.classList.add("dark");
+
+    themeBtn.innerHTML='<i class="fa-solid fa-sun"></i>';
+
+}
+
+themeBtn.onclick=()=>{
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+
+        localStorage.setItem("theme","dark");
+
+        themeBtn.innerHTML='<i class="fa-solid fa-sun"></i>';
+
+    }else{
+
+        localStorage.setItem("theme","light");
+
+        themeBtn.innerHTML='<i class="fa-solid fa-moon"></i>';
+
+    }
+
+};
