402c5698c0745f033cdd517b3a551a87778c5a7b
tawnieraka-blip <tawnie.raka@gmail.com>
2026-07-01 20:15:11 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
+function updateClock(){
 
+const now=new Date();
+
+const days=[
+"الأحد",
+"الاثنين",
+"الثلاثاء",
+"الأربعاء",
+"الخميس",
+"الجمعة",
+"السبت"
+];
+
+const months=[
+"يناير",
+"فبراير",
+"مارس",
+"أبريل",
+"مايو",
+"يونيو",
+"يوليو",
+"أغسطس",
+"سبتمبر",
+"أكتوبر",
+"نوفمبر",
+"ديسمبر"
+];
+
+document.getElementById("clock").innerHTML=
+now.toLocaleTimeString('ar-EG');
+
+document.getElementById("day").innerHTML=
+days[now.getDay()];
+
+document.getElementById("date").innerHTML=
+`${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()}`;
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
