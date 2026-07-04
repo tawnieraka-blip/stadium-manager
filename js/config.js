21f2c2a0f1e5c107d3f090fdcc31983c67898308
tawnieraka-blip <tawnie.raka@gmail.com>
2026-07-04 17:47:46 +0300

    Delete js/config.js

--- js/config.js
+++
@@ -1,9 +0,0 @@
-/*=========================================
- Stadium Manager Config
-=========================================*/
-
-const CONFIG = {
-
-    API_URL: "https://script.google.com/macros/s/AKfycbyyQqVuDr29OMrCuBmWo8EBGmMH9-sXvu8CF4YDCk0CugoKnAQEjFfOtoVSBRK9wVkoBQ/exec"
-
-};
